0f67c1d745253137033875af562ccb87e81490eb
Carlos Alberto <carlosssilva.filho@gmail.com>
2025-02-26 19:37:32 -0300

    Somando dois numeros

---
+++ day-2/somar_dois_num.py
@@ -0,0 +1,12 @@
+def somar(numero1, numero2):
+    soma_dois_numeros = numero1 + numero2 
+    return soma_dois_numeros
+#somar(34,55)
+
+def somar_dois_num():
+    numero1 = int(input("Colocar o primero numero: "))
+    numero2 = int(input("Colocar o segundo número :"))
+    somar_dois_numeros = numero1 +  numero2
+    print(somar_dois_numeros)
+
+somar_dois_num()
---
+++ day-2/somar_dois_numeros.py
@@ -0,0 +1,7 @@
+numero1 = input("Digite um numero: ")
+numero2 = input("Digite outro numero: ")
+
+soma_dois_numeros = int(numero1) + int(numero2)
+
+print(f"Essa é a soma de dois números em python : {soma_dois_numeros}")
+

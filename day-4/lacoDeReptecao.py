1bb841e5b3458127930938857e6c72c8dba9df62
Carlos Alberto <carlosssilva.filho@gmail.com>
2025-03-01 17:02:14 -0300

    Adicionando o dia 4 laço de repetição

---
+++ day-4/lacoDeReptecao.py
@@ -0,0 +1,10 @@
+# unsando o for para imprimir de 0 a 10 
+
+for numero in range(11): 
+    print(numero)
+    
+    # usando while para imprimir nuemro 0 a 10 
+    numero = 0 
+    while numero < 11: 
+        print(numero)
+        numero +=1 
